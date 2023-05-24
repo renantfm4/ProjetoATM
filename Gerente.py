30205c92fb79ccd3f80080a3d2cb703139c2bc0b
DeM4rcio <henriquemarcio170@gmail.com>
2023-05-23 23:30:29 -0300

    modified gerente

--- Gerente.py
+++ Gerente.py
@@ -1,1 +1,16 @@
-#
+class Gerente():
+#Criação da classe gerente para melhor visualização, focar na classe conta e usuário!!!
+    def RegistrarConta():
+        pass
+    def deletarConta():
+        pass
+    def ListarContas():
+        pass
+
+class Usuario():
+    def __init__(self, nome, cpf, telefone, endereco):
+        self.nome = nome
+        self.cpf = cpf,
+        self.telefone = telefone
+        self.endereco = endereco,
+        
