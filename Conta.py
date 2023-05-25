9429104adc4cb02d4fec3b079ecc7b8d8d1b74dc
*Renan <renantfm0@gmail.com>
2023-05-25 16:04:19 -0300

    atualizando

--- Conta.py
+++ Conta.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Conta:
     def __init__(self, senha, instituicao, saldo, limite, extrato):
         self.senha = senha
@@ -19,3 +21,24 @@ class Conta:
             print("Depósito realizado!")
         else:
             print("Valor inserido é inválido!")
+
+    def PagamentoProgramado(self, destinatario):
+        self.destinatario = destinatario
+        a = datetime.now()
+        hora_atual = a.strftime("%H:%M")
+        print("Hora atual: ", hora_atual)
+        print("Para qual horário você quer realizar o pagamento?")
+        self.horario_programado = input()
+        print("O quanto você quer transferir?")
+        self.valor = int(input())
+        if self.valor > 0 and self.valor <= self.saldo + self.limite:
+            self.saldo -= self.valor
+            destinatario.saldo += self.valor
+            print(f"Pagamento no valor de {self.valor} R$ programado para às {self.horario_programado}")
+        else:
+            print("Saldo insuficiente!")
+
+        #if hora_atual == self.horario_programado:
+           # print("Pagamento programado realizado!")
+        #else:
+          #  print("Fora do horário para o pagamento!")
--- Front.py
+++ Front.py
@@ -1,18 +1,21 @@
 # ESSE ARQUIVO ESTÁ SENDO APENAS NO MOMENTO PRA TESTAR O FRONT
 
 from Conta import Conta
+import datetime
 
 # Criar instância da classe Conta
 minha_conta = Conta(senha="senha123", instituicao="Banco XYZ", saldo=1000, limite=500, extrato=[])
+conta_destinatario = Conta(senha="senha456", instituicao="Banco ABC", saldo=0, limite=0, extrato=[])
+
 
 
 
 # Chamar a função sacar() para fazer um saque
-minha_conta.sacar(valor=200)
+#minha_conta.sacar(valor=200)
 
 # Acessar os atributos atualizados
-print("Saldo atual:", minha_conta.saldo)
-print("Extrato atual:", minha_conta.extrato)
+#print("Saldo atual:", minha_conta.saldo)
+#print("Extrato atual:", minha_conta.extrato)
 
 
 # Chamar a função depositar() para fazer um depósito
@@ -20,3 +23,15 @@ print("Extrato atual:", minha_conta.extrato)
 
 # Acessar o saldo atualizado
 #print("Saldo atual:", minha_conta.saldo)
+
+# Chamar a função transferir() para realizar uma transferência
+minha_conta.PagamentoProgramado(destinatario=conta_destinatario)
+
+# Acessar os atributos atualizados
+print("Saldo atual da minha conta:", minha_conta.saldo)
+print("Saldo atual da conta destinatário:", conta_destinatario.saldo)
+print("Extrato atual da minha conta:", minha_conta.extrato)
+print("Extrato atual da conta destinatário:", conta_destinatario.extrato)
+
+
+#minha_conta.PagamentoProgramado()
