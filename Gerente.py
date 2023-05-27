57dd00df7dbc96687a146002b74d34da714906ae
DeM4rcio <henriquemarcio170@gmail.com>
2023-05-26 22:13:36 -0300

    add

--- Gerente.py
+++ Gerente.py
@@ -1,17 +1,21 @@
 import json
 
-
+# Carregando o arquivo json dentro da variavel "dados"
 with open("C:\WORKSPACE\Python\ATM\Banco_de_dados.json",  encoding="utf-8") as arquivo:
             dados = json.load(arquivo)
+# Variavel que direcionára as modificação no arquivo json
+carregar_arquivo = open("C:\WORKSPACE\Python\ATM\Banco_de_dados.json","w")
+
+
 
 class Gerente():
 #Criação da classe gerente para melhor visualização, focar na classe conta e usuário!!!
     def RegistrarConta():
 
-        nome, cpf, telefone, endereco = input("Insira os dados do cliente! (nome), (cpf), (telefone), (endereco)").split(" ")               
+        nome, cpf, telefone, endereco = input("Insira os dados do cliente! (nome), (cpf), (telefone), (endereco)").split(" ")                      
 
-        carregar_arquivo = open("C:\WORKSPACE\Python\ATM\Banco_de_dados.json","w")
         dados.append({"nome":nome, "cpf":cpf, "telefone":telefone, "endereco":endereco})
+        
         json.dump(dados, carregar_arquivo, indent=6)
 
         
@@ -19,10 +23,19 @@ class Gerente():
          
 
     def deletarConta():
-        nome, senha = input("Coloque o nome do cliente e suas senha para a remoção").split(" ")
-
-        for i in dados:
-            print(dados[i])
+        nome, cpf = input("Coloque o nome do cliente e suas senha para a remoção").split(" ")
+        try:
+            for i in range(len(dados)):
+                if (dados[i].get("nome") == nome) and (dados[i].get("cpf") == cpf):
+                    dados.pop(i)
+                    json.dump(dados, carregar_arquivo, indent=6)
+                else:
+                     pass
+        except IndexError:
+             print("Usuário não encontrado!")
+                 
+                 
+       
 
 
 
