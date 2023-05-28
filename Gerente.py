70987f31ae37da219bdf5a9b2f3c11aade1ea0a4
DeM4rcio <henriquemarcio170@gmail.com>
2023-05-27 22:16:26 -0300

    add II

--- Gerente.py
+++ Gerente.py
@@ -1,16 +1,20 @@
 import json
+import shutil
+import tempfile
+from glob import glob
 
-# Carregando o arquivo json dentro da variavel "dados"
 with open("C:\WORKSPACE\Python\ATM\Banco_de_dados.json",  encoding="utf-8") as arquivo:
             dados = json.load(arquivo)
-# Variavel que direcionára as modificação no arquivo json
-carregar_arquivo = open("C:\WORKSPACE\Python\ATM\Banco_de_dados.json","w")
-
 
 
 class Gerente():
-#Criação da classe gerente para melhor visualização, focar na classe conta e usuário!!!
+
+    
     def RegistrarConta():
+
+        
+        # Variavel que direcionára as modificação no arquivo json
+        carregar_arquivo = open("C:\WORKSPACE\Python\ATM\Banco_de_dados.json","w")
 
         nome, cpf, telefone, endereco = input("Insira os dados do cliente! (nome), (cpf), (telefone), (endereco)").split(" ")                      
 
@@ -23,24 +27,26 @@ class Gerente():
          
 
     def deletarConta():
-        nome, cpf = input("Coloque o nome do cliente e suas senha para a remoção").split(" ")
+    
         try:
+            nome, cpf = input("Coloque o nome do cliente e suas senha para a remoção\n").split(" ")
             for i in range(len(dados)):
                 if (dados[i].get("nome") == nome) and (dados[i].get("cpf") == cpf):
+                    carregar_arquivo = open("C:\WORKSPACE\Python\ATM\Banco_de_dados.json","w")
                     dados.pop(i)
                     json.dump(dados, carregar_arquivo, indent=6)
                 else:
-                     pass
-        except IndexError:
-             print("Usuário não encontrado!")
-                 
+                    print("Usuário não encontrado!, verifique os dados informados")
+        except ValueError:
+            print("Dados fornecidos incorretamente!")        
                  
        
 
 
 
     def ListarContas():
-        pass
+        for i in range(len(dados)):
+            print(dados[i])
 
 class Usuario():
     def __init__(self, nome, cpf, telefone, endereco):
@@ -50,4 +56,4 @@ class Usuario():
         self.endereco = endereco,
         
 gerente1 = Gerente
-gerente1.deletarConta()
+gerente1.ListarContas()
