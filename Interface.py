7a00bcd26e3f72d1feb7c73c946193251bab7bcb
*Renan <renantfm0@gmail.com>
2023-06-04 19:36:56 -0300

    atualizando

--- Interface.py
+++ Interface.py
@@ -52,16 +52,34 @@ class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.configuracoes()
-        self.tela_login()
+        self.tipo()
+        
     
     def configuracoes(self):
         self.geometry(f"{700}x{400}")
         self.title("Sistema de Login")
         self.resizable(0, 0)
         self.configure(bg="black")
+        
 
-    def tela_login(self):
+    def tipo(self):
+        self.frame_tipo = ctk.CTkFrame(self, width=350, height=380)
+        self.frame_tipo.place(x=250, y=80)
 
+        self.otipo = ctk.CTkLabel(self.frame_tipo, text="Que tipo você é?", font=("Comic Sans MS", 20))
+        self.otipo.grid(row=0, column=0, padx=10, pady=10)
+
+        self.btn_gerente_sub = ctk.CTkButton(self.frame_tipo, text="Gerente".upper(), command=
+        self.tela_login_gerente)
+        self.btn_gerente_sub.grid(row=1, column=0, padx=10, pady=10)
+        self.btn_usuario_sub = ctk.CTkButton(self.frame_tipo, text="Usuário".upper(), command=
+        self.tela_login_usuario)
+        self.btn_usuario_sub.grid(row=3, column=0, padx=10, pady=10)
+        
+
+    def tela_login_usuario(self):
+
+        self.frame_tipo.place_forget()
         img = Image.open("login.png")
         img = img.resize((300, 300))  # Redimensiona a imagem para 200x200 pixels
         
@@ -80,10 +98,10 @@ class App(ctk.CTk):
         self.lab_title = ctk.CTkLabel(self.frame_login, text="Faça o seu Login", font=("Century Gothic bold", 22))
         self.lab_title.grid(row=1, column=0, padx=10, pady=10)
 
-        self.username_login_entry = ctk.CTkEntry(self.frame_login, width=300, placeholder_text="Digite seu CPF")
+        self.username_login_entry = ctk.CTkEntry(self.frame_login, width=300, placeholder_text="Digite seu CPF", corner_radius=15)
         self.username_login_entry.grid(row=2, column=0, padx=10, pady=10) 
 
-        self.senha_login_entry = ctk.CTkEntry(self.frame_login, width=300, placeholder_text="Digite sua senha")
+        self.senha_login_entry = ctk.CTkEntry(self.frame_login, width=300, placeholder_text="Digite sua senha", corner_radius=15, show="*")
         self.senha_login_entry.grid(row=3, column=0, padx=10, pady=10)
         
         self.ver_senha = ctk.CTkCheckBox(self.frame_login, text="Clique no checkbox para visualizar a senha")
@@ -91,6 +109,50 @@ class App(ctk.CTk):
 
         self.btn_login_sub = ctk.CTkButton(self.frame_login, text="Login".upper(), width=300, corner_radius=15)
         self.btn_login_sub.grid(row=5, column=0, padx=10, pady=10)
+
+    def tela_login_gerente(self):
+
+        self.frame_tipo.place_forget()
+        img = Image.open("login.png")
+        img = img.resize((350, 350))  # Redimensiona a imagem para 200x200 pixels
+        
+        photo = ImageTk.PhotoImage(img)
+
+        self.frame_login_gerente2 = ctk.CTkFrame(self, width=900, height=900)
+        self.frame_login_gerente2.place(x=0, y=0)
+        
+        self.lb_img = ctk.CTkLabel(self.frame_login_gerente2, text=None, image=photo)
+        self.lb_img.grid(row=2, column=0, padx=3)
+        
+        self.title = ctk.CTkLabel(self.frame_login_gerente2, text="Login de Gerente", font=("Comic Sans MS", 20))
+        self.title.grid(row=0, column=0, pady=10)
+
+
+        self.frame_login_gerente = ctk.CTkFrame(self, width=350, height=380)
+        self.frame_login_gerente.place(x=368, y=80)
+
+        self.lab_title = ctk.CTkLabel(self.frame_login_gerente, text="Faça o seu Login", font=("Century Gothic bold", 22))
+        self.lab_title.grid(row=1, column=0, padx=10, pady=10)
+
+        self.username_login_entry = ctk.CTkEntry(self.frame_login_gerente, width=300, placeholder_text="Digite seu CPF", corner_radius=15)
+        self.username_login_entry.grid(row=2, column=0, padx=10, pady=10) 
+
+        self.senha_login_entry = ctk.CTkEntry(self.frame_login_gerente, width=300, placeholder_text="Digite sua senha", corner_radius=15, show="*")
+        self.senha_login_entry.grid(row=3, column=0, padx=10, pady=10)
+        
+        self.ver_senha = ctk.CTkCheckBox(self.frame_login_gerente, text="Clique no checkbox para visualizar a senha")
+        self.ver_senha.grid(row=4, column=0, padx=10, pady=10)
+
+        self.btn_login_sub = ctk.CTkButton(self.frame_login_gerente, text="Login".upper(), width=300, corner_radius=15,
+        command=self.menu_gerente)
+        self.btn_login_sub.grid(row=5, column=0, padx=10, pady=10)
+    
+    def menu_gerente(self):
+        self.frame_login_gerente.place_forget()
+        self.frame_login_gerente2.place_forget()
+
+        self.menu = ctk.CTkFrame(self, width=350, height=380)
+        self.menu.place(x=350, y=80)
 
 
 if __name__ == "__main__":
